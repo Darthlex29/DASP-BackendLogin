c2f770aad469a38442e97c12de96bf8357b7f7aa
Darthlex29 <david.dam37@gmail.com>
2023-11-23 08:57:51 -0500

    Funtional Update

--- Backend/src/config.py
+++ Backend/src/config.py
@@ -6,7 +6,7 @@ class Config:
     SECRET_KEY = config('SECRET_KEY')
     
     SQLALCHEMY_DATABASE_URI = config('SQLALCHEMY_DATABASE_URI') or \
-        'mysql://udwocckwfv06wzu9:B4rSteiAKzbEvLL3Laui@brs91absj0pvf3xov5we-mysql.services.clever-cloud.com/brs91absj0pvf3xov5we'
+        'mysql+mysqlconnector://udwocckwfv06wzu9:B4rSteiAKzbEvLL3Laui@brs91absj0pvf3xov5we-mysql.services.clever-cloud.com/brs91absj0pvf3xov5we'
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
