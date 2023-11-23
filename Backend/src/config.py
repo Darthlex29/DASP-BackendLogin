c15079e4e2afafbe718e2b217def57fd6d3f914d
Darthlex29 <david.dam37@gmail.com>
2023-11-23 13:00:14 -0500

    configUpdate

--- Backend/src/config.py
+++ Backend/src/config.py
@@ -2,7 +2,7 @@ import os
 import secrets
 
 class Config:
-    SECRET_KEY = 'SECRET_KEY'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_hex(32)
     
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://udwocckwfv06wzu9:B4rSteiAKzbEvLL3Laui@brs91absj0pvf3xov5we-mysql.services.clever-cloud.com/brs91absj0pvf3xov5we'
     
